3a43c62383eda988893c5a2305ecbc398c017724
SanghyunPark <shpark@mv-w.com>
2024-04-14 22:12:28 +0900

    ♻ REFACTOR: urlpatterns 수정

--- pp_enter_be/config/urls.py
+++ pp_enter_be/config/urls.py
@@ -4,15 +4,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-def hello_world(request):
-    return HttpResponse("Hello, World!")
-
 urlpatterns = [
-    path('api/v1/admin/', admin.site.urls),
-    path("api/v1/facechats/", include("facechats.urls")),
-    path("api/v1/posts/", include("posts.urls")),
-    path("api/v1/accounts/", include("accounts.urls")),
-    path("", hello_world)
+    path('admin/', admin.site.urls),
+    path("facechats/", include("facechats.urls")),
+    path("posts/", include("posts.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
