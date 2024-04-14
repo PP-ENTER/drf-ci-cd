97497c841b049d3cc9afa24bc1ad8c1d7a14fb41
SanghyunPark <shpark@mv-w.com>
2024-04-14 21:34:44 +0900

    WIP

--- pp_enter_be/config/urls.py
+++ pp_enter_be/config/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+def hello_world(request):
+    return HttpResponse("Hello, World!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("facechats/", include("facechats.urls")),
     path("posts/", include("posts.urls")),
     path("accounts/", include("accounts.urls")),
+    path("", hello_world)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
