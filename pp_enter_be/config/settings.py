8c61be0320bd73dc94e7a2b03913e5c27621be15
SanghyunPark <shpark@mv-w.com>
2024-04-17 01:31:36 +0900

    dude

--- pp_enter_be/config/settings.py
+++ pp_enter_be/config/settings.py
@@ -168,7 +168,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'http://localhost:3000',
-    'https://ppnenter.com/',
+    'https://ppnenter.com',
     'https://www.ppnenter.com',
     # Add more allowed origins as needed
 ]
