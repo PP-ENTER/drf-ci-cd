52c237b412fec1a4f4b6f5f3441ef6e8d853f0ef
Sanghyun Park <59405184+daisybum@users.noreply.github.com>
2024-04-17 02:25:07 +0900

    Update settings.py

--- pp_enter_be/config/settings.py
+++ pp_enter_be/config/settings.py
@@ -167,9 +167,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
-    'http://localhost:3000',
-    'https://ppnenter.com',
-    'https://www.ppnenter.com',
+    'http://localhost:3000'
     # Add more allowed origins as needed
 ]
 
