d5329c6bb40d3da7cfde12a4924f3237e8350f34
Sanghyun Park <59405184+daisybum@users.noreply.github.com>
2024-04-16 21:50:42 +0900

    Update settings.py

--- pp_enter_be/config/settings.py
+++ pp_enter_be/config/settings.py
@@ -164,7 +164,11 @@ SIMPLE_JWT = {
 }
 
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://localhost:3000',
+    # Add more allowed origins as needed
+]
 
 CORS_ALLOW_HEADERS = [
     'accept',
